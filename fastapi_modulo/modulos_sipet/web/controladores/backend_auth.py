0be026b4b1281f1720095160d73e23b428874d38
jalm2035gmail <jalm2035@gmail.com>
2026-03-25 13:48:13 -0600

    Tolerar ausencia de user_admin en backend auth

--- fastapi_modulo/modulos_sipet/web/controladores/backend_auth.py
+++ fastapi_modulo/modulos_sipet/web/controladores/backend_auth.py
@@ -5,10 +5,16 @@ from fastapi import APIRouter
 from fastapi_modulo.modulos_sipet.web.controladores.auth_api import router as auth_api_router
 from fastapi_modulo.modulos_sipet.web.controladores.auth_pages import router as auth_pages_router
 from fastapi_modulo.modulos_sipet.web.controladores.auth_passkey import router as auth_passkey_router
-from fastapi_modulo.modulos_sipet.web.controladores.user_admin import router as user_admin_router
 
 router = APIRouter()
 router.include_router(auth_pages_router)
 router.include_router(auth_api_router)
 router.include_router(auth_passkey_router)
-router.include_router(user_admin_router)
+
+try:
+    from fastapi_modulo.modulos_sipet.web.controladores.user_admin import router as user_admin_router
+except ModuleNotFoundError:
+    user_admin_router = None
+
+if user_admin_router is not None:
+    router.include_router(user_admin_router)
